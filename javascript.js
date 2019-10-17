124c8839ea280c918416c88e9736721ee3082c18
player <1032050177@qq.com>
2019-10-17 18:41:54 +0800

    第三次提交

--- javascript.js
+++ javascript.js
@@ -8,3 +8,22 @@ function bark() {
     console.log('woof')
 }
 bark.animal = 'dog'
+
+//3
+function test(){
+    var n=4399;
+    function add(){
+        n++;
+        console.log(n);
+    }
+    return {
+        n:n,
+        add:add
+    }
+}
+var result=test();
+var result2=test();
+result.add();
+result.add();
+console.log(result.n);
+result2.add();
